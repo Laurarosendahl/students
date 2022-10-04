7beda011d0431bc4a9c10bf1a4031bb2281b4d00
Laurarosendahl <90824564+Laurarosendahl@users.noreply.github.com>
2022-10-04 10:22:26 +0200

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,19 @@
+const cacheName = 'cache-students'; 
+
+//Når websitet indlæses, cache ressourcer nævnt i listen 
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(["/students/", "/students/index.html",  "/students/javascript.js","/students/members.json", 
+            "/students/mystyle.css", "/students/morten.png", "/students/nina.png", "/students/olivia.png"]);
+        })
+    )
+})
+
+//Hvis ressource ikke er tilgængelig online, så søg i cachen efter et match
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        fetch(event.request).catch(() =>
+        caches.open(cacheName).then(cache=>cache.match(event.request)))
+    )
+})
